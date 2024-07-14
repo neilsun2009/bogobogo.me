e24997e7da6ab7f1f26e64aef093c539d572a213
neilsun2009 <neilsun2009@163.com>
2024-07-14 13:24:35 +0800

    feat(server): upgrade to https sever

--- bogobogo-server/server.js
+++ bogobogo-server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
@@ -7,6 +9,10 @@ const port = 9999;
 // Serve static files from the "static" directory
 app.use(express.static(path.join(__dirname, 'static')));
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// HTTPS server
+https.createServer({
+  key: fs.readFileSync('./CA/2_bogobogo.cn.key'),
+  cert: fs.readFileSync('./CA/1_bogobogo.cn_bundle.crt')
+}, app).listen(port, () => {
+  console.log(`Server is running at https://localhost:${port}`);
 });
